79475995bb99195ae9e141d6a77b6f1897a3394c
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-01-12 23:43:07 +0200

    initial upload

---
+++ src/components/NextButton.js
@@ -0,0 +1,29 @@
+import { useQuiz } from "../contexts/QuizContext";
+
+function NextButton() {
+  const { dispatch, answer, index, numQuestions } = useQuiz();
+
+  if (answer === null) return null;
+
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "newQuestion" })}
+      >
+        Next
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+}
+
+export default NextButton;
